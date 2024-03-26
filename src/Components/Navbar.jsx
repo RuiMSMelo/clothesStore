28b8dfe4ec45b6f5d975ff22bda7c2cb7e06ffa8
RuiMSMelo <ruimsmelo@gmail.com>
2024-03-26 11:16:51 +0100

    removed wishlist link to=

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,62 +5,62 @@ import Wishlist from "../imgs/heart-icon.png";
 import { CartContext } from "../Contexts/Cart";
 
 const Navbar = () => {
-	const { cartItems } = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
 
-	return (
-		<nav>
-			<div className="navbar">
-				<div className="navbarBrand">
-					<Link to={"/"} className="navbar-brand">
-						<h1>The Store</h1>
-					</Link>
-				</div>
-				<div className="navbarButtonsGroup">
-					<div className="navbarLeftButtonsGroup">
-						<Link to="/mens">
-							<button>Men</button>
-						</Link>
-						<Link to="/womens">
-							<button>Women</button>
-						</Link>
-						<Link to="/jewelery">
-							<button>Jewelry</button>
-						</Link>
-						<Link to="/electronics">
-							<button>Electronics</button>
-						</Link>
-					</div>
-					<div className="navbarRightButtonsGroup">
-						<Link to="/wishlist">
-							<img src={Wishlist} className="navbarIcons" />
-						</Link>
-						<div className="cartLink">
-							<Link to="/cart">
-								<img src={cartIcon} className="navbarIcons" />
-								<p className="cartItems-length">({cartItems.length})</p>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="second-navbar">
-				<div>
-					<p>The Store card - get up to 50% off</p>
-				</div>
-				<div>
-					<p>Free standard shipping over 50€</p>
-				</div>
-				<div>
-					<p>
-						Shop now, pay later.{" "}
-						<Link style={{ color: "black", textDecoration: "underline" }}>
-							Learn more
-						</Link>
-					</p>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav>
+      <div className="navbar">
+        <div className="navbarBrand">
+          <Link to={"/"} className="navbar-brand">
+            <h1>The Store</h1>
+          </Link>
+        </div>
+        <div className="navbarButtonsGroup">
+          <div className="navbarLeftButtonsGroup">
+            <Link to="/mens">
+              <button>Men</button>
+            </Link>
+            <Link to="/womens">
+              <button>Women</button>
+            </Link>
+            <Link to="/jewelery">
+              <button>Jewelry</button>
+            </Link>
+            <Link to="/electronics">
+              <button>Electronics</button>
+            </Link>
+          </div>
+          <div className="navbarRightButtonsGroup">
+            <Link>
+              <img src={Wishlist} className="navbarIcons" />
+            </Link>
+            <div className="cartLink">
+              <Link to="/cart">
+                <img src={cartIcon} className="navbarIcons" />
+                <p className="cartItems-length">({cartItems.length})</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="second-navbar">
+        <div>
+          <p>The Store card - get up to 50% off</p>
+        </div>
+        <div>
+          <p>Free standard shipping over 50€</p>
+        </div>
+        <div>
+          <p>
+            Shop now, pay later.{" "}
+            <Link style={{ color: "black", textDecoration: "underline" }}>
+              Learn more
+            </Link>
+          </p>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
